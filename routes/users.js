9e5bf770e19e5e4293c09a9fc546a6cc8fe72194
liruifengv <li15227870916@gmail.com>
2017-12-25 16:37:15 +0800

    login api

--- routes/users.js
+++ routes/users.js
@@ -5,42 +5,87 @@ var jwt = require('jsonwebtoken');  // token
 var expressJwt = require('express-jwt'); // express token
 let Users = require('../models/users')
 
-// 获取 userInfo
-router.get('/users/:id', function(req, res, next) {
-  let userId = req.params.id
-  Users.findById(userId).then(function (user) {
-		return res.status(200).json(user.userInfo);
-	}).catch(function (err) {
-		return res.status(401).send();
-	});
-})
+  // 用户注册
+  router.post('/users', function(req, res ,next) {
 
-// 用户注册
-router.post('/users', function(req, res ,next) {
-  let username = req.body.username
-  let password = req.body.password
-  // console.log(req.body.username)
-  // 加密
-  let md5 = crypto.createHash('md5')
-  let hashedPassword = md5.update(password).digest('hex')
+    let username = req.body.username
+    let password = req.body.password
 
-  let newUser = {
-    username: username,
-    hashedPassword: hashedPassword
-  }
-  
-  Users.findOne({username: username})
+    // 加密
+    let md5 = crypto.createHash('md5')
+    let hashedPassword = md5.update(password).digest('hex')
+    
+    let newUser = {
+      username: username,
+      hashedPassword: hashedPassword
+    }
+
+    Users.findOne({username: username})
+      .then((userInfo) => {
+        if (userInfo) {
+          res.status(400).send({'content':'用户已存在'});
+        } else {
+          Users.create(newUser)
+            .then((newUserInfo) => {
+              res.sendStatus(200)
+            })
+        }
+      })
+  })
+
+  // 用户登录
+  router.post('/login', function (req, res, next) {
+
+    let username = req.body.username
+    let password = req.body.password
+
+    Users.findOne({username: username})
     .then((userInfo) => {
       if (userInfo) {
-        res.status(400).send({'content':'用户已存在'});
+        // 加密
+        let md5 = crypto.createHash('md5')
+        let hashedPassword = md5.update(password).digest('hex')
+        // 判断密码正确
+        if (hashedPassword === userInfo.hashedPassword) {
+          let sayhub_token = jwt.sign({ username: username, userID: userInfo.id }, 'sayhub')
+          return res.status(200).send({'sayhub_token': sayhub_token});
+        } else {
+          res.status(400).send({'content':'密码错误'});
+        }
       } else {
-        Users.create(newUser)
-          .then((newUserInfo) => {
-            res.sendStatus(200)
-          })
+        res.status(400).send({'content':'用户不存在'});
       }
     })
-})
+  })
 
+  // 获取 userInfo
+  router.get('/users/:id', function(req, res, next) {
+    let userId = req.params.id
+    Users.findById(userId).then(function (user) {
+      return res.status(200).json(user.userInfo);
+    }).catch(function (err) {
+      return res.status(401).send();
+    });
+  })
+
+  // 获取登陆用户 Profile
+  router.get('/profile', function(req, res, next) {
+    // console.log(req.headers.authorization)
+    let sayhub_token = req.headers.authorization.slice(7)
+    // console.log(sayhub_token)
+    if (sayhub_token) {
+      const decoded = jwt.verify(sayhub_token, 'sayhub');
+      // 从token中拿到用户名和userID
+      const username = decoded.username
+      const userID = decoded.userID
+      Users.findById(userID).then(function (user) {
+        return res.status(200).json(user.userInfo);
+      }).catch(function (err) {
+        return res.status(401).send();
+      });
+    } else {
+      return res.status(401).send();
+    }
+  })
 
 module.exports = router;

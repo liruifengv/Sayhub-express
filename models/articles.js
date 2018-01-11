06dfa4a8e2e2372bb92ae6c0e6686c87ae855faa
liruifengv <li15227870916@gmail.com>
2018-01-11 17:23:22 +0800

    article edit&&votes

--- models/articles.js
+++ models/articles.js
@@ -25,6 +25,24 @@ let articlesSchema = new Schema({
   category: {
     type: Array
   },
+  is_up: {             // 是否点赞
+    type: Boolean,
+    default: false
+  },
+  votes_count: {             // 点赞数量
+    type: Number,
+    default: 0
+  },
+  votes: {                  // 点赞人数组(里面包含点赞人的 id)
+    type: Array
+  },
+  comments_count: {          // 评论数量
+    type: Number,
+    default: 0
+  },
+  comments: {          // 评论数组(里面包含评论的 id)
+    type: Array
+  },
   created: {
     type: String
   },
--- routes/articles.js
+++ routes/articles.js
@@ -1,5 +1,6 @@
 let express = require('express')
 let router = express.Router()
+var jwt = require('jsonwebtoken');  // token
 let Articles = require('../models/articles')
 var md = require('markdown-it')({
   html: true,
@@ -64,7 +65,6 @@ router.post('/articles', function (req, res, next) {
   const content_text = req.body.content.replace(reg, "")
   let newArticle = {
     title: req.body.title,
-    abstract: req.body.abstract,
     content: req.body.content,
     content_render: content_render,
     content_text: content_text, 
@@ -81,14 +81,51 @@ router.post('/articles', function (req, res, next) {
 
 router.get('/article/:id', function(req, res, next) {
   let _id = req.params.id
-  // console.log(req.params)
-  Articles.findById({_id: _id})
-  .then((ArticleDetail) => {
-    // console.log(ArticleDetail)
-    res.json(ArticleDetail)
-  }).catch(function (err) {
-    return res.status(401).send()
-  })
+
+  if (req.headers.authorization) {
+    console.log('已登录')
+    let sayhub_token = req.headers.authorization.slice(7)
+    const decoded = jwt.verify(sayhub_token, 'sayhub');
+    // 从token中拿到用户名和userID
+    // const username = decoded.username
+    const userID = decoded.userID
+    Articles.findById(_id)
+      .then((info) => {
+        let _votes = info.votes
+        if (_votes.includes(userID)) {
+          Articles.findByIdAndUpdate(_id, {
+            is_up: true
+          },{new: true})
+          .then((ArticleDetail) => {
+            // console.log(ArticleDetail)
+            res.json(ArticleDetail)
+          }).catch(function (err) {
+            return res.status(401).send()
+          })
+        } else {
+          Articles.findByIdAndUpdate(_id, {
+            is_up: false
+          },{new: true})
+          .then((ArticleDetail) => {
+            // console.log(ArticleDetail)
+            res.json(ArticleDetail)
+          }).catch(function (err) {
+            return res.status(401).send()
+          })
+        }
+      })
+  } else {
+    console.log('未登录')
+    Articles.findByIdAndUpdate(_id, {
+      is_up: false
+    },{new: true})
+    .then((ArticleDetail) => {
+      // console.log(ArticleDetail)
+      res.json(ArticleDetail)
+    }).catch(function (err) {
+      return res.status(401).send()
+    })
+  }
 })
 
 router.delete('/article/:id', function(req, res, next) {
@@ -100,6 +137,77 @@ router.delete('/article/:id', function(req, res, next) {
   }).catch(function (err) {
       return res.status(401).send()
   })
+})
+
+router.put('/article/:id', function (req, res, next) {
+  let _id = req.params.id
+  // console.log(req.body)
+  const reg = /[\\\`\*\_\[\]\#\+\-\!\>]/g
+  const content_render = md.render(req.body.content);
+  const content_text = req.body.content.replace(reg, "")
+  let editArticle = {
+    title: req.body.title,
+    content: req.body.content,
+    content_render: content_render,
+    content_text: content_text, 
+    author: req.body.author,
+    category: req.body.category,
+    updated: new Date().toLocaleString()
+  }
+  Articles.findByIdAndUpdate(_id, editArticle, {new:true})
+    .then((ArticleInfo) => {
+      return res.status(200).json({id: ArticleInfo._id})
+    })
+})
+
+router.post(`/article/:id/up` , function(req, res, next) {
+
+  let articleId = req.params.id
+  
+  let sayhub_token = req.headers.authorization.slice(7)
+  // console.log(sayhub_token)
+  
+  if (sayhub_token) {
+    const decoded = jwt.verify(sayhub_token, 'sayhub');
+    // 从token中拿到用户名和userID
+    const username = decoded.username
+    const userID = decoded.userID
+
+    Articles.findByIdAndUpdate(articleId,{
+      '$addToSet':{'votes':userID},
+      '$inc':{'votes_count':1},
+      is_up: true
+    },{new:true}).then((ArticleInfo) => {
+      res.status(200).json({votes_count: ArticleInfo.votes_count})
+    })
+  } else {
+    return res.sendStatus(401)
+  }
+})
+
+router.delete(`/article/:id/up` , function(req, res, next) {
+
+  let articleId = req.params.id
+  
+  let sayhub_token = req.headers.authorization.slice(7)
+  // console.log(sayhub_token)   
+  
+  if (sayhub_token) {
+    const decoded = jwt.verify(sayhub_token, 'sayhub');
+    // 从token中拿到用户名和userID
+    const username = decoded.username
+    const userID = decoded.userID
+
+    Articles.findByIdAndUpdate(articleId,{
+      'pull':{'votes':userID},
+      '$inc':{'votes_count':-1},
+      is_up: false
+    },{new:true}).then((ArticleInfo) => {
+      res.status(200).json({votes_count: ArticleInfo.votes_count})
+    })
+  } else {
+    return res.sendStatus(401)
+  }
 })
 
 module.exports = router;
